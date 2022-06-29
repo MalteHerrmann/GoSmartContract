0fad18036cd5d9715690be0affb282a175906357
Malte Herrmann <malteherrmann.mail@web.de>
2022-06-30 00:24:10 +0200

    Add basic contract for testing purposes

---
+++ contracts/Maltcoin_temp.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/// @title Maltcoin
+/// @author Malte Herrmann
+/** @notice This contract only contains a public variable name 
+to demonstrate successful deployment to a local Evmos node. 
+*/
+contract Maltcoin {
+    string public name = "Maltcoin";
+}
