755abfc4a78f297b1feab18208efaddc32b62963
Malte Herrmann <malteherrmann.mail@web.de>
2022-06-30 00:23:41 +0200

    Add NatSpec comments

--- contracts/Maltcoin.sol
+++ contracts/Maltcoin.sol
@@ -1,13 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-// Import IERC20 interface from OpenZeppelin contracts library
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// Define the contract
+/// @title Maltcoin
+/// @author Malte Herrmann
+/// @notice This contract defines an ERC20 token called Maltcoin
+/** @dev This contract was generated using the OpenZeppelin contract 
+wizard: https://wizard.openzeppelin.com/
+*/ 
 contract Maltcoin is ERC20 {
-    // Constructor
-    constructor() ERC20("Maltcoin", "Malt") {
+    /** @notice The constructor function is called upon deployment of the
+    contract. It initializes the contract with the name and symbol 
+    of the token.
+    */ 
+    /// @dev 10.000 tokens are minted and assigned to the transaction sender.
+    constructor() ERC20("Maltcoin", "MALT") {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 }
